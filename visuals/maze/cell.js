07a5830bae0cf314459ebcdc8f5f39c44a96bc62
StanaAndrei <stadey33@gmail.com>
2021-05-12 17:13:34 +0300

    maze almost ready

--- visuals/maze/cell.js
+++ visuals/maze/cell.js
@@ -1,32 +1,69 @@
-const LEN = 20;
 class Cell {
-  constructor(i, j) {
+  constructor (i, j) {
     this.i = i;
     this.j = j;
-    this.walls = new Array(4).fill(true);
+    this.walls = [true, true, true, true];
     this.visited = false;
   }
+  getNext() {
+    var neighbors = [];
+    let i = this.i;
+    let j = this.j;
+    var top = grid[index(i, j - 1)];
+    var right = grid[index(i + 1, j)];
+    var bottom = grid[index(i, j + 1)];
+    var left = grid[index(i - 1, j)];
+
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbors.push(left);
+    }
+
+    if (neighbors.length > 0) {
+      var r = floor(random(0, neighbors.length));
+      return neighbors[r];
+    } else {
+      return undefined;
+    }
+  };
+
+  highlight(color) {
+    var x = this.i * w;
+    var y = this.j * w;
+    noStroke();
+    fill(color);
+    rect(x, y, w, w);
+  };
+
   draw() {
-    let x = this.i * LEN;
-    let y = this.j * LEN;
+    var x = this.i * w;
+    var y = this.j * w;
     stroke(255);
     if (this.walls[0]) {
-      line(x, y, x + LEN, y);
+      line(x, y, x + w, y);
     }
     if (this.walls[1]) {
-      line(x + LEN, y, x + LEN, y + LEN);
+      line(x + w, y, x + w, y + w);
     }
     if (this.walls[2]) {
-      line(x + LEN, y + LEN, x, y + LEN);
+      line(x + w, y + w, x, y + w);
     }
     if (this.walls[3]) {
-      line(x, y + LEN, x, y);
+      line(x, y + w, x, y);
     }
-    //check vis
+
     if (this.visited) {
-        noStroke();
-        fill('rgb(50,205,50)');
-        rect(x, y, LEN, LEN);
+      noStroke();
+      fill(255, 0, 255, 100);
+      rect(x, y, w, w);
     }
-  }
+  };
 }
--- visuals/maze/sketch.js
+++ visuals/maze/sketch.js
@@ -1,24 +1,75 @@
-let board;
-const n = 25;
-const SIZE = 500;
+var cols, rows;
+var w = 20;
+var grid = [];
+
+var current;
+
+var stack = [];
 
 function setup() {
-    console.log(SIZE);
-    let canvas = createCanvas(SIZE, SIZE );
-    canvas.position(100, 100);
-    board = new Array(n).fill().map(() => []);
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            board[i].push(new Cell(i, j));
-        }
-    }//*/
+  createCanvas(600, 600);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  //frameRate(15);
+
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
+    }
+  }
+
+  current = grid[0];
 }
 
 function draw() {
-    background(220);
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            board[i][j].draw();
-        }
-    }//*/
+  background(1);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].draw();
+  }
+
+  current.visited = true;
+  current.highlight(color(252, 3, 3));
+  // STEP 1
+  var next = current.getNext();
+  if (next) {
+    next.visited = true;
+
+    // STEP 2
+    stack.push(current);
+
+    // STEP 3
+    removeWalls(current, next);
+
+    // STEP 4
+    current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
+  }
+}
+
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+  return i + j * cols;
+}
+
+function removeWalls(a, b) {
+  var x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  var y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
 }
