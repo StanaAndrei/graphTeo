cc47048445e53ca32d9a41ca73a287721b7b88f9
StanaAndrei <stadey33@gmail.com>
2021-05-12 22:21:33 +0300

    op1u1

--- visuals/maze/opscript.js
+++ visuals/maze/opscript.js
@@ -1,17 +1,18 @@
 let genBtn = document.querySelector('#submit');
 let radio15 = document.querySelector('#op1');
 var frames;
-const DEBUG = true;
+const DEBUG = false;
 genBtn.addEventListener('click', (e) => {
     e.preventDefault();
     if (radio15.checked) {
         frames = 15;
         let mazeURL = new String();
         mazeURL += window.location.hostname;
+        console.log(window.location.hostname);
         if (DEBUG) {
             mazeURL += ':5500';
         }
-        mazeURL += '/visuals/maze/maze.html';
+        mazeURL += '/graphTeo/visuals/maze/maze.html';
         window.location.href = mazeURL;
     }//*/
 })
