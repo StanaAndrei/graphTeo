5e82ba8033f5ec82bd840d903de4fae7d93ca3d4
StanaAndrei <stadey33@gmail.com>
2021-05-15 12:01:30 +0300

    rel2

--- visuals/relationdis/rellogic.js
+++ visuals/relationdis/rellogic.js
@@ -1,5 +1,6 @@
 let from, to;
 let allActors;;
+let edges = [];
 const getMovies = async () => {
     const url = './data.json';
     let res = await fetch(url);
@@ -7,10 +8,37 @@ const getMovies = async () => {
     return json.movies;
 }
 //--------------------------------------
+
+const mkdefined = (actor) => {
+    if (edges[actor] == undefined) {
+        edges[actor] = [];
+        console.log(1);
+    }
+}
+
 const getLink = async (from, to) => {
     let movies = await getMovies();
     from = this.from;
     to = this.to;
     console.log(movies[0]);
+    for (let i = 0; i < movies.length; i++) {
+        let title = movies[i].title;
+        let cast = movies[i].cast;
+        for (let i = 0; i < cast.length; i++) {
+            for (let j = i + 1; j < cast.length; j++) {
+                mkdefined(cast[i]);
+                mkdefined(cast[j]);
+                edges[cast[i]].push({
+                    to: cast[j],
+                    movie: title,
+                });
+                edges[cast[j]].push({
+                    to: cast[i],
+                    movie: title,
+                });
+            }
+        }
+    }
+    //console.log(edges['Steve Guttenberg']);
     return 1;
 }
