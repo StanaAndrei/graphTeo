c1df218faf5ec4ca97ae63221e4283cd32a2650c
StanaAndrei <stadey33@gmail.com>
2021-05-19 14:46:59 +0300

    main canv

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    console.log('sketch ok!');
+    canvas.position(0, 0);
+    canvas.style('z-index', -1);
+    
+}
+
+function draw() {
+    background(148, 177, 227);
+}
