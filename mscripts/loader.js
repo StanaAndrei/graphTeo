0a107c3f8ade0b2fc87b800f6e57b89735935d77
StanaAndrei <stadey33@gmail.com>
2021-05-24 21:29:34 +0300

    fixed remote home

--- mscripts/loader.js
+++ mscripts/loader.js
@@ -1,12 +1,12 @@
 const loadPartials= async () => {
-    let partials = document.querySelectorAll('.partial');
+    var partials = document.querySelectorAll('.partial');
     for (let partial of partials) {
         const path = partial.getAttribute('data-path');
         let res = await fetch(path);
         let text = await res.text();
         partial.innerHTML = text;//*/.
     }
-    //console.log('loaded');
+    
 };
 
 (async () => {
