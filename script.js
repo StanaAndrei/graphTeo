87bc16dbdd62086238a860d9287c3d219a819b79
StanaAndrei <stadey33@gmail.com>
2021-05-27 21:46:37 +0300

    +btn fn

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 "use strict";
 const buttons = document.querySelectorAll('.mainmenu > .button');
 const iniCol = buttons[0].style.backgroundColor;
-const mPages = [
-    ''
+const DEBUG = Boolean(window.location.href.search('https') === -1);
+const pages = [
+    './pages/theo/theory.html',
+    './pages/pbs/pbs.html',
+    './pages/visuals/visuals.html'
 ];
 
 window.onload = async () => {
+    if (!DEBUG) {
+        pages.map(page => '/graphTeo/' + page);
+    }
     console.log('works');
     await loadPartials();
-    for (let button of buttons) {
-        button.addEventListener('mouseover', (event) => {
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        button.addEventListener('mouseover', event => {
             event.target.style.backgroundColor = 'blue';
         });
-        button.addEventListener('mouseout', (event) => {
+        button.addEventListener('mouseout', event => {
             event.target.style.backgroundColor = iniCol;
         });
-        button.addEventListener('click', (event) => {
-            
+        button.addEventListener('click', event => {
+            window.location.assign(pages[i]);
         });
     }
 }
